97b0ad774647dca6f7d015b46f12c89ee0b3d82c
zyh <zhang.yuh@outlook.com>
2024-10-30 15:33:50 +0800

    add run sh

---
+++ run_pd_ddpg.sh
@@ -0,0 +1,11 @@
+python pendulum/DDPG/ddpg.py
+python pendulum/DDPG/ddpg.py
+python pendulum/DDPG/ddpg.py
+python pendulum/DDPG/ddpg.py
+python pendulum/DDPG/ddpg.py
+
+python pendulum/DDPG-CBF/ddpg.py
+python pendulum/DDPG-CBF/ddpg.py
+python pendulum/DDPG-CBF/ddpg.py
+python pendulum/DDPG-CBF/ddpg.py
+python pendulum/DDPG-CBF/ddpg.py
